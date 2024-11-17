a6d64d8dad23639ef36cd88dc560a7b0f3f9a8b7
semako123 <zosusemenoch@gmail.com>
2024-11-17 14:39:14 +0100

    Update box sizing

--- src/components/containers/LargestSales.jsx
+++ src/components/containers/LargestSales.jsx
@@ -3,7 +3,7 @@ import Slider from "../Slider";
 
 const LargestSales = () => {
   return (
-    <div className="p-4 lg:p-20 lg:-mb-20 xl:p-32 text-[#101111] bg-white">
+    <div className="p-4 lg:p-20 lg:-mb-20 xl:p-24 text-[#101111] bg-white">
       <h3 className="text-4xl lg:text-5xl font-semibold my-10">Largest Sales</h3>
       <Slider
         url="https://api.opensea.io/api/v2/collection/cryptopunks/nfts?limit=80"
--- src/components/containers/RecentTransactions.jsx
+++ src/components/containers/RecentTransactions.jsx
@@ -3,7 +3,7 @@ import Slider from "../Slider";
 
 const RecentTransactions = () => {
   return (
-    <div className="p-4 lg:p-20 xl:p-32 pb-10 text-[#101111] bg-white">
+    <div className="p-4 lg:p-20 xl:p-24 pb-10 text-[#101111] bg-white">
       <h3 className="text-4xl lg:text-5xl font-semibold my-10">
         Recent Transactions
       </h3>
